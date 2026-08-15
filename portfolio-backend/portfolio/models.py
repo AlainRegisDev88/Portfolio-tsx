09912c812299d8b2a983f8c856006f57f34324c1
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-08-15 20:10:15 +0200

    add the education model

--- portfolio-backend/portfolio/models.py
+++ portfolio-backend/portfolio/models.py
@@ -51,8 +51,21 @@ class Experience(models.Model):
     name = models.CharField(unique=True)
     category = models.CharField(max_length=50)
     organisation = models.CharField(max_length=50, blank=False)
-    start_date = models.DateField(blank=False)
-    end_date = models.DateField(blank=True)
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
     
     def __str__(self):
         return self.name
+    
+class Education(models.Model):    
+    uuid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    school = models.CharField(max_length=70)
+    program = models.CharField(max_length=70)
+    location = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
+    coursework = ArrayField(models.CharField(max_length=50), blank=True, default=list)
+    
+    
+    
