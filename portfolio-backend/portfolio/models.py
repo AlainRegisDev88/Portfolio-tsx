43d632993949126cc8f944dccff8999fb3010324
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-08-15 15:20:56 +0200

    update the skills model

--- portfolio-backend/portfolio/models.py
+++ portfolio-backend/portfolio/models.py
@@ -1,3 +1,5 @@
+import uuid
+from django.core.validators import MinValueValidator, MaxValueValidator 
 from django.db import models
 
 # Create your models here.
@@ -11,16 +13,29 @@ class Skills(models.Model):
         ('tools', 'Tools & Platforms')
     ]
     
-    
+    uuid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
     name = models.CharField(max_length=50, unique=True)
     category = models.CharField(
         max_length=50,
         choices= CATEGORY_CHOICES,
         default= 'programming'
     )
+    proficiency =models.IntegerField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(100)
+        ]
+    )
+    currently_learning = models.BooleanField(default=False, null=True)
+    
 
     def __str__(self):
         return self.name
 
 
+class Projects(models.Model):
+    uuid  =models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    name = models.CharField(max_length=50, unique=True)
+    
+    
     
