346c1274d7a88d12aba525396dc83820859b1c8a
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-08-15 22:56:38 +0200

    register the modules into admin

--- portfolio-backend/portfolio/admin.py
+++ portfolio-backend/portfolio/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Education, Experience, Profile, Projects, Skills 
+
+admin.site.register(Education)
+admin.site.register(Experience)
+admin.site.register(Profile)
+admin.site.register(Projects)
+admin.site.register(Skills)
