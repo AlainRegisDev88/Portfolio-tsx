1f29453d6ccb60dc2497e7298ce80aacdff51190
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-08-15 14:58:36 +0200

    add Skills model

--- portfolio-backend/portfolio/models.py
+++ portfolio-backend/portfolio/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Skills(models.Model):
+    
+    CATEGORY_CHOICES = [
+        ('programming', 'Programming'),
+        ('AI and data science', 'AI & Data Science'),
+        ('web_development', 'Web Development'),
+        ('tools', 'Tools & Platforms')
+    ]
+    
+    
+    name = models.CharField(max_length=50, unique=True)
+    category = models.CharField(
+        max_length=50,
+        choices= CATEGORY_CHOICES,
+        default= 'programming'
+    )
+
+    def __str__(self):
+        return self.name
+
+
+    
