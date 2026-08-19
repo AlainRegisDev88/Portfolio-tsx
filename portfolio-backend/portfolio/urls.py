6dc6fc8e7ab72bf8980011e9436f79316d926cb7
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-08-19 20:09:13 +0200

    add the skills api endpoint

--- portfolio-backend/portfolio/urls.py
+++ portfolio-backend/portfolio/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('greetings', views.hello_world, name="hello-world")
+    path('greetings', views.hello_world, name="hello-world"),
+    path('skills', views.get_skills, name="skills")
 ]
+
