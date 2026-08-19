87e0748f8f8648809fc11c385287c24f4908e13b
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-08-19 22:53:34 +0200

    add skills services

---
+++ portfolio-frontend/src/services/skillsServices.ts
@@ -0,0 +1,18 @@
+import api from "./api";
+
+
+const skillsServices = {
+
+    getSkills: async() => {
+        try {
+            const result = await api.get('/skills')
+            return result
+        } catch (error) {
+            console.log(error)
+            throw error   
+        }
+
+    },
+}
+
+export default skillsServices;
