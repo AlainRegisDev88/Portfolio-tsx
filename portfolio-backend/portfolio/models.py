f9ab86b8c34f7c73c2087fb3325462343b508e08
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-08-20 17:02:36 +0200

    add description field to experience model

--- portfolio-backend/portfolio/models.py
+++ portfolio-backend/portfolio/models.py
@@ -66,6 +66,7 @@ class Experience(models.Model):
     name = models.CharField(unique=True)
     category = models.CharField(max_length=50)
     organisation = models.CharField(max_length=50, blank=False)
+    description = models.TextField(default="")
     start_date = models.DateField()
     end_date = models.DateField(null=True, blank=True)
     
