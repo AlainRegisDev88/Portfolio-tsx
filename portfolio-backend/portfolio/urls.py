6612861a4924c515a2cf09307b625633e1c622a9
Alain Regis Udahemuka <ualainregis@gmail.com>
2026-08-20 12:13:46 +0200

    update url patterns for api endpoints

--- portfolio-backend/portfolio/urls.py
+++ portfolio-backend/portfolio/urls.py
@@ -1,11 +1,19 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+router.register(r'projects', views.ProjectsViewSet, basename='project')
 
 urlpatterns = [
-    path('greetings', views.hello_world, name="hello-world"),
-    path('skills', views.get_skills, name="skills"),
-    path('education', views.get_education, name="education"),
-    path('profile', views.get_profile, name='profile'),
+    path('api/greetings', views.hello_world, name="hello-world"),
+    path('api/skills', views.get_skills, name="skills"),
+    path('api/education', views.get_education, name="education"),
+    path('api/profile', views.get_profile, name='profile'),
+    
+    
+    path('api/', include(router.urls))
     
 ]
 
